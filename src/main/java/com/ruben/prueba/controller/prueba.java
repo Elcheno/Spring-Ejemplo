6ebe7cdd178f2dc0d221c475e02f453e483be735
Ruben <rubenca2312@gmail.com>
2023-03-28 00:12:31 +0200

    push3

--- src/main/java/com/ruben/prueba/controller/prueba.java
+++ src/main/java/com/ruben/prueba/controller/prueba.java
@@ -8,9 +8,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class prueba {
 
-    @GetMapping("/prueba/{nombre}")
-    public String welcomeDefault(@PathVariable String nombre, Model model){
-        model.addAttribute("persona", nombre);
+    @GetMapping("/prueba/{str}/{id}")
+    public String p1(@PathVariable String str, @PathVariable int id, Model model){
+        model.addAttribute("persona", str);
+        model.addAttribute("id", id);
         return "prueba";
     }
+
+    @GetMapping("/prueba2/{str}/{id}/{cod}")
+    public String p2(@PathVariable String str,
+                     @PathVariable int id,
+                     @PathVariable int cod,
+                     Model model){
+        model.addAttribute("str", str);
+        model.addAttribute("id", id);
+        model.addAttribute("cod", cod);
+        return "prueba2";
+    }
+
 }
